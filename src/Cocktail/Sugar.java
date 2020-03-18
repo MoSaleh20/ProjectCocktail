e72077538906a978b5a07b585f60d03c91f512bb
97259 <97259@MohammadSaleh>
2020-03-19 01:07:47 +0200

    First commitment i do ever

---
+++ src/Cocktail/Sugar.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cocktail;
+
+/**
+ *
+ * @author 97259
+ */
+public class Sugar extends Ingredients {
+
+    public Sugar() {
+    }
+
+    public Sugar(String name, double volume) {
+        super(name);
+        
+        this.calories=volume*38;
+    }
+    
+}
